05371854160fea44d2f96c3ec8f70b900f7476e4
Nailatul <nailatuliftori@student.telkomuniversity.ac.id>
2025-01-02 15:38:30 +0700

    mapel

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MapelController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return redirect()->route('login');
-});
 
-// Rute untuk login dan logout menggunakan AuthController
-Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('login', [AuthController::class, 'login']);
-Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+Route::resource('mapels', MapelController::class);
 
-// Rute untuk dashboard dan halaman utama setelah login
-Route::get('/dashboard', function () {
-    // Redirect langsung ke halaman students
-    return redirect()->route('students.index');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Rute untuk profil
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__ . '/auth.php';
