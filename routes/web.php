6589adbc0024a2cb5d116b4570473cf5d6b64de1
ElvinaKristanti <elvinakristanti@gmail.com>
2025-01-02 15:20:31 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,29 @@
 <?php
 
-
-use App\Http\Controllers\MapelController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return redirect()->route('login');
+});
 
-    Route::resource('mapels', MapelController::class);
+// Rute untuk login dan logout menggunakan AuthController
+Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+
+// Rute untuk dashboard dan halaman utama setelah login
+Route::get('/dashboard', function () {
+    // Redirect langsung ke halaman students
+    return redirect()->route('students.index');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+// Rute untuk profil
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
